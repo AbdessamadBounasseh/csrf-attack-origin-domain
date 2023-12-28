289821c29342b94cb469e61f760fe26fa67be1cf
Abdessamad Bounasseh <abdessamad.bounasseh@uit.ac.ma>
2023-12-28 10:49:30 +0100

    Before securing our application

---
+++ src/main/java/com/csrf/csrf/controller/BankController.java
@@ -0,0 +1,28 @@
+package com.csrf.csrf.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Slf4j
+public class BankController {
+
+    @GetMapping("/transfer")
+    @ResponseBody
+    public String transfer(@RequestParam("accountNo") int accountNo,
+                           @RequestParam("amount") final int amount) {
+        log.info("Transfer {} to {}", amount, accountNo);
+        return "Transfer "+amount+" to "+accountNo;
+    }
+
+    @PostMapping("/transfer")
+    @ResponseStatus(HttpStatus.OK)
+    public void transfer2(@RequestParam("accountNo") int accountNo,
+                          @RequestParam("amount") final int amount) {
+        log.info("Transfer {} to {}", amount, accountNo);
+    }
+}
